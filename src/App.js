3a07943aeb6dbcf66fcdc51e7070d0d39efca62c
Arely Contreras <icoca@Arelys-iMac.local>
2021-08-09 07:29:05 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Iconos from './components/Iconos'
+import Botones from './components/Botones'
+import { DatePicker, Button } from 'antd';
+//importaciones de la hoja de estilos
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+//importaciones de iconos de ant-desing
+import { Html5TwoTone, StarOutlined, StarFilled, StarTwoTone, AppleOutlined, ChromeOutlined, DeleteFilled, HeartTwoTone } from '@ant-design/icons';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <DatePicker style={{ fontSize: '90px', width: '30%' }} />
+      <br></br>
+      <br></br>
+      <Iconos />
+      <br></br>
+      <Botones />
+
+    </div>
+  );
+}
+
+export default App;
