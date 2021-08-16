ba7eab090412b8638e7effdea0e4aff2d945bbd9
vicenterivero2021 <valdhair@oasishotel.com>
2021-08-16 12:51:23 -0500

    masteds

---
+++ src/components copy/Botones copy.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button } from 'antd';
+import { Html5TwoTone, LineOutlined, SearchOutlined} from '@ant-design/icons';
+function Botones() {
+    return (
+        <div>
+            {/* Ghost hacer que el boton sea transparente de fondo */}
+            <h2>Botones de Ant desing</h2>
+            <Button type="dashed" icon={<Html5TwoTone/>} size="large">dashed</Button>
+            <Button type="primary" size="large">Primary</Button>
+            <Button type="link" href="https://www.google.com" target="_blank" size="large">Google</Button>
+            <Button type="ghost" size="large">ghost</Button>
+
+            <h2>Botones adicionales  de Ant desing</h2>
+            <Button type="dashed" danger size="small">danger</Button>
+            <Button type="primary" danger    loading size="small">Primary loading</Button>
+            <Button type="ghost" disabled size="small">disabled</Button>
+            <Button ghost size="large">ghost</Button>
+            <Button type="primary" ghost size="large">ghost-primary</Button>            <Button type="primary" ghost size="large">ghost-primary</Button>
+            <Button type="primary" ghost size="large">ghost-primary</Button>
+
+            <Button type="primary" icon={<SearchOutlined/>} ghost shape="circle" size="large"></Button>
+            <Button type="primary" icon={<SearchOutlined/>} ghost className="botonAnt"></Button>
+
+
+
+
+        </div>
+    )
+}
+export default Botones
